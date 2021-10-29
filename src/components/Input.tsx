bb353f76fb75a4e76a7e3586c70a8645f9ef0a63
DavideMono <davide@mono.studio>
2021-10-29 10:37:59 +0200

    feat(input): add external style support

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,22 @@
 import React, { VFC } from 'react'
-import { StyleSheet, TextInput } from 'react-native'
+import { StyleProp, StyleSheet, TextInput, TextStyle } from 'react-native'
 import { BACKGROUND_COLOR, BORDER_RADIUS, LIGHT_COLOR } from 'src/lib/constant'
 
 type Props = {
   value: string
   onChange: (nextValue: string) => void
   placeholder?: string
+  style?: StyleProp<TextStyle>
 }
 
 const Input: VFC<Props> = (props) => {
   return (
-    <TextInput style={styles.input} value={props.value} onChangeText={props.onChange} placeholder={props.placeholder} />
+    <TextInput
+      style={[styles.input, props.style]}
+      value={props.value}
+      onChangeText={props.onChange}
+      placeholder={props.placeholder}
+    />
   )
 }
 
