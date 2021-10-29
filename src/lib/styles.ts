2a3b30e25c9086ea4d24e46d5ab82b2d83ad6ad4
DavideMono <davide@mono.studio>
2021-10-29 11:49:56 +0200

    feat(home): general layout

---
+++ src/lib/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export const COMMON_STYLE = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  subtitleLight: {
+    fontSize: 20
+  },
+  link: {
+    color: '#EC7669',
+    textDecorationLine: 'underline'
+  }
+})
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,69 @@
-import React, { VFC } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, VFC } from 'react'
+import { Keyboard, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
+import Input from 'src/components/Input'
+import Button from 'src/components/Button'
+import { SECONDARY } from 'src/lib/constant'
+import { COMMON_STYLE } from 'src/lib/styles'
 
 const Home: VFC = () => {
+  const [value, setValue] = useState<string>('')
+
   return (
-    <View style={styles.root}>
-      <Text>Hello world!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.root}>
+        <Input style={styles.component} value={value} onChange={setValue} placeholder="Search" />
+        <View style={styles.component}>
+          <Text style={COMMON_STYLE.title}>Hello,</Text>
+          <Text style={COMMON_STYLE.subtitleLight}>$YOUR NAME</Text>
+        </View>
+        <View style={[styles.component, styles.cardComponent]}>
+          <View style={[styles.cardLeftChild, styles.cardTextContainer]}>
+            <Text style={COMMON_STYLE.subtitle}>Your Plan for today</Text>
+            <Text>$NC of $NT completed</Text>
+            <Text style={[COMMON_STYLE.link, styles.textSpacer]}>Show more</Text>
+          </View>
+          <View style={styles.cardRightChild} />
+        </View>
+        <Text style={[COMMON_STYLE.subtitle]}>Daily Review</Text>
+        <ScrollView>
+          <Text>NO TASK FOR NOW</Text>
+        </ScrollView>
+        <Button
+          styleRoot={styles.component}
+          color="secondary"
+          onPress={() => console.log('On press')}
+          text="Press me"
+        />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 const styles = StyleSheet.create({
   root: {
+    padding: 12
+  },
+  component: {
+    marginVertical: 8
+  },
+  cardComponent: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: 'row',
+    backgroundColor: SECONDARY,
+    paddingHorizontal: 8,
+    paddingVertical: 12
+  },
+  cardLeftChild: {
+    flex: 1
+  },
+  cardRightChild: {
+    flex: 1
+  },
+  cardTextContainer: {
+    display: 'flex'
+  },
+  textSpacer: {
+    marginTop: 24
   }
 })
 
