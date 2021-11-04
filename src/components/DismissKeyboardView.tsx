d1876711be8ed00a1b65ce53e0ddae62176d59b7
DavideMono <davide@mono.studio>
2021-11-04 17:56:14 +0100

    perf(keyboard-layout): integrate bottom navigation

--- src/components/DismissKeyboardView.tsx
+++ src/components/DismissKeyboardView.tsx
@@ -8,23 +8,28 @@ import {
   View,
   ViewStyle
 } from 'react-native'
+import BottomNavigation from 'src/components/BottomNavigation'
 
 type Props = {
+  navigation?: boolean
   style?: StyleProp<ViewStyle>
 }
 
-const DismissKeyboardView: FC<Props> = (props) => {
+const DismissKeyboardView: FC<Props> = ({ navigation = true, ...props }) => {
   return (
     <KeyboardAvoidingView behavior="height" style={styles.root}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={[styles.root, props.style]}>{props.children}</View>
+        <View style={[styles.root, props.style]}>
+          {props.children}
+          {navigation && <BottomNavigation />}
+        </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
-  root: { flex: 1 }
+  root: { flex: 1, padding: 8 }
 })
 
 export default DismissKeyboardView
